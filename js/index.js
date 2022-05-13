4ffc48d75f23bb835440e0038fdd6b23dfe722f3
jonnebonde <jonnebonde@gmail.com>
2022-05-13 12:11:45 +0200

    enter

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-const toggleButton = document.querySelector(".toggle-button");
+const toggleButton = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+
 toggleButton.addEventListener("click", () => {
-    navMenu.classList.toggle("active")
+    toggleButton.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
 })
+
