c1f12c9770a36f743d4aed215c202665fcdc5fb2
jonnebonde <jonnebonde@gmail.com>
2022-05-27 22:00:35 +0200

    enter

--- js/components/random.js
+++ js/components/random.js
@@ -15,15 +15,14 @@ selected.forEach(function (random) {
     popularProductsContainer.innerHTML +=
         `
     <div class="random-product" >
-    <a href="details.html?id=${random.id}&name=${random.name}&gender=${random.gender}" data-product="${random.id}">
+    <a tabindex="-1" href="details.html?id=${random.id}&name=${random.name}&gender=${random.gender}" data-product="${random.id}">
         <h3>${random.name}</h3>
     
-    <a href="details.html?id=${random.id}&name=${random.name}&gender=${random.gender}" data-product="${random.id}">
+    <a tabindex="-1" href="details.html?id=${random.id}&name=${random.name}&gender=${random.gender}" data-product="${random.id}">
         <div style="background-image: url(${random.image})" aria-label="a ${random.gender} is wearing a ${random.name}" class="random-product-image"></div>
     </a>
-             <span>${random.description}</span>
+            <span>${random.description}</span>
             <span class="product-price">Price: ${random.price}</span>
-           
             <a href="details.html?id=${random.id}&name=${random.name}" data-product="${random.id}" class="random-product-button">Details</a>
         </div>
         `
